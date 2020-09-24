557657fb37e5745e1cdef77ffc23af3965395b84
Bluesxz <1252455742@qq.com>
2020-09-25 00:07:22 +0800

    feat:两两交换链表节点

---
+++ Week_01/24.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+/*
+24. 两两交换链表中的节点
+https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//递归版本
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	first := head
+	second := first.Next
+	first.Next = swapPairs(second.Next)
+	second.Next = first
+	return second
+}
+
+//循环遍历版本
+
+func swapPairs2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	ret := head.Next
+	pre := &ListNode{Next: head}
+	for head != nil && head.Next != nil {
+		first := head
+		second := first.Next
+		third := second.Next
+
+		//second->first->third
+		//pre->second
+		second.Next = first
+		first.Next = third
+		pre.Next = second
+
+		//pre->first
+		//head->third
+		pre = first
+		head = third
+	}
+
+	return ret
+}
+
+func printList(head *ListNode) {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
--- Week_01/main.go
+++ Week_01/main.go
@@ -15,4 +15,11 @@ func main() {
 	nums = []int{0, 1, 0, 3, 12}
 	moveZeroes2(nums)
 	fmt.Println(nums)
+
+	node4 := &ListNode{4, nil}
+	node3 := &ListNode{3, node4}
+	node2 := &ListNode{2, node3}
+	node1 := &ListNode{1, node2}
+	head := swapPairs2(node1)
+	printList(head)
 }
