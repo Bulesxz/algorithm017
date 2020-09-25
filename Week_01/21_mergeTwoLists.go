202c05aa35247199d9ff72d61a7d6ae840725ae6
Bluesxz <1252455742@qq.com>
2020-09-25 22:41:58 +0800

    调整文件名

--- Week_01/21_mergeTwoLists.go
+++ Week_01/21_mergeTwoLists.go
@@ -17,8 +17,6 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		return l2
 	} else if l2 == nil {
 		return l1
-	} else if l1 == nil && l2 == nil {
-		return nil
 	}
 	var head *ListNode //新链表的头
 	if l1.Val <= l2.Val {
@@ -48,4 +46,19 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return head
 
+}
+
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+	if l1.Val <= l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
 }
