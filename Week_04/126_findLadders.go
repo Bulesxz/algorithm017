76e187cb2567d77c74a248ad0ec9f65ffa1af30e
shi <1252455742@qq.com>
2020-10-22 19:47:56 +0800

    合并区间

--- Week_04/126_findLadders.go
+++ Week_04/126_findLadders.go
@@ -5,48 +5,48 @@ package main
 https://leetcode-cn.com/problems/word-ladder-ii/description/
 */
 
-func findLadders(beginWord string, endWord string, wordList []string) [][]string {
-	wordmap := genChangeA(len(beginWord), wordList)
-	// fmt.Println(wordmap)
-	visited := map[string]bool{}
-	level := 0
-	queue := []string{beginWord}
-	visited[beginWord] = true
-	for len(queue) > 0 {
-		//遍历队列
-		level++
-		curSize := len(queue)
-		for i := 0; i < curSize; i++ {
-			top := queue[0]
-			queue = queue[1:] //出栈
+// func findLadders(beginWord string, endWord string, wordList []string) [][]string {
+// 	wordmap := genChangeA(len(beginWord), wordList)
+// 	// fmt.Println(wordmap)
+// 	visited := map[string]bool{}
+// 	level := 0
+// 	queue := []string{beginWord}
+// 	visited[beginWord] = true
+// 	for len(queue) > 0 {
+// 		//遍历队列
+// 		level++
+// 		curSize := len(queue)
+// 		for i := 0; i < curSize; i++ {
+// 			top := queue[0]
+// 			queue = queue[1:] //出栈
 
-			next := map[string]bool{}
-			for k := 0; k < len(top); k++ {
-				c := []byte(top) // 将字符串 s 转换为 []byte 类型
-				c[k] = '*'
-				newWord := string(c)
-				if t, ok := wordmap[newWord]; ok {
-					for key := range t {
-						next[key] = true
-					}
-				}
-			}
-			// fmt.Println("next", next)
+// 			next := map[string]bool{}
+// 			for k := 0; k < len(top); k++ {
+// 				c := []byte(top) // 将字符串 s 转换为 []byte 类型
+// 				c[k] = '*'
+// 				newWord := string(c)
+// 				if t, ok := wordmap[newWord]; ok {
+// 					for key := range t {
+// 						next[key] = true
+// 					}
+// 				}
+// 			}
+// 			// fmt.Println("next", next)
 
-			for w := range next {
-				if _, ok := visited[w]; ok {
-					//访问过
-					continue
-				}
-				if endWord == w {
-					return level + 1
-				}
-				visited[w] = true
-				queue = append(queue, w)
-				// fmt.Println("queue", queue)
-			}
+// 			for w := range next {
+// 				if _, ok := visited[w]; ok {
+// 					//访问过
+// 					continue
+// 				}
+// 				if endWord == w {
+// 					return level + 1
+// 				}
+// 				visited[w] = true
+// 				queue = append(queue, w)
+// 				// fmt.Println("queue", queue)
+// 			}
 
-		}
-	}
-	return 0
-}
+// 		}
+// 	}
+// 	return 0
+// }
