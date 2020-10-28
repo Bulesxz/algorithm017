1289b8a6b76bf4980651efd39a5de5fe552c7bf6
shi <1252455742@qq.com>
2020-10-29 00:17:49 +0800

    凑零钱  递归缓存法

--- Week_06/322_coinChange.go
+++ Week_06/322_coinChange.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 /*
 322. 零钱兑换
 https://leetcode-cn.com/problems/coin-change/
@@ -7,23 +11,40 @@ https://leetcode-cn.com/problems/coin-change/
 
 /*
 方法一
-递归: 每一个硬币选或者必选 爬楼梯
+递归: 每一个硬币选或者必选 类似爬楼梯
 */
-func recursiveCoinChange() {
-
-}
-func coinChange(coins []int, amount int) int {
+func recursiveCoinChange(coins []int, amount int, memo map[int]int) int {
+	if res, ok := memo[amount]; ok {
+		return res
+	}
 	//1. 递归终止
 	if amount < 0 {
-		return
+		memo[amount] = -1
+		return -1
 	}
-	for coin := range coins {
-		a := coinChange(coins, amount-coin)
-		b := coinChange(coins, amount)
-		if a < b {
-			max = a
-		} else {
-			max = b
+	if amount == 0 {
+		memo[amount] = 0
+		return 0
+	}
+	min := math.MaxInt32
+	for _, coin := range coins {
+		// fmt.Println(amount)
+		res := recursiveCoinChange(coins, amount-coin, memo)
+		// fmt.Println("amount", amount, "min:", min, coin)
+		if res >= 0 && res < min {
+			min = res + 1 // 加1，是为了加上得到res结果的那个步骤中，兑换的一个硬币
 		}
 	}
+
+	if min == math.MaxInt32 {
+		memo[amount] = -1
+		return -1
+	} else {
+		memo[amount] = min
+		return min
+	}
+}
+func coinChange(coins []int, amount int) int {
+	memo := map[int]int{}
+	return recursiveCoinChange(coins, amount, memo)
 }
--- Week_06/main.go
+++ Week_06/main.go
@@ -7,4 +7,7 @@ func main() {
 	n := 2
 	res := uniquePaths2(m, n)
 	fmt.Println(res)
+	coins := []int{1, 2, 5}
+	res2 := coinChange(coins, 100)
+	fmt.Println(res2)
 }
